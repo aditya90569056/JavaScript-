12b50e03ca7be177e0f978b50696626f79db65be
aditya nath <126580063+aditya90569056@users.noreply.github.com>
2026-06-09 00:22:37 +0530

    d

---
+++ chapter2-ES6/func.js
@@ -0,0 +1,35 @@
+// const outer=(a)=>(b)=>(c)=>a+b+c;
+// console.log(outer(1)(2)(10));
+// const adi = (name)=>(role)=>(dept)=>`hello ${name} you'r a ${role} in ${dept}`
+// console.log(adi("aditya")("Ai engineer")("Open Ai"));
+// let obj={
+//     a,
+//     b,
+//     c
+// }
+// console.log(obj);
+// function sample(name,age){
+    // this.name = name;
+    // this.age  = age;
+// }
+// adi = new  sample("aditya",20);
+// aman = new sample("aman",20);
+// console.log(adi.name);
+// this. = function(){
+//     console.log("wow");
+// }
+// console.log(this);
+// wow();
+// func=(name = "aditya")=>{
+// console.log(name);
+// }
+// func("aman");
+// function count(val = false){
+//     if(val){
+//         for(i=1;i<=5;){
+//             console.log(`Count: ${i}`);
+//             i++;
+//         }
+//     }
+// }
+// count(1);
