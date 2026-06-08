bb14bc7f61713d13aaf136d5007e430b4d2fbeca
aditya nath <126580063+aditya90569056@users.noreply.github.com>
2026-06-08 23:37:48 +0530

    11:37PM 8/6/26

--- chapter1/functions.js
+++ chapter1/functions.js
@@ -1,4 +1,24 @@
-console.log(abc());
+// console.log(abc());
+// function abc(){
+//     return "hello";
+// }
+
+// abc = () =>{  
+//       console.log("oppo");
+// }
+// abc();
+// val=()=>2
+// sum=(i)=>Math.pow(i,2);
+// show=(val)=>{
+    // console.log(sum(val));
+// }
+// console.log(sum());
+// setInterval(show(8),6000);
+var i = 0
 function abc(){
-    return "hello";
+   return  function inner(){
+        return i+1;
+    }
 }
+ccd = abc();
+console.log(ccd());
--- chapter1/loops.js
+++ chapter1/loops.js
@@ -17,4 +17,22 @@
 // if(true || false){
 // console.log("T");
 // }
-var arr=[1,2,3,4,5];
+// var arr=[1,2,3,4,5];
+arr=[1,2,3,4]
+// arr.pop();
+// arr.push(5);
+// arr.shift();
+// arr.unshift(0);
+//console.log(arr.includes(2));
+// name = "aditya";
+// console.log(arr.join("").split(""));
+// console.log(name.reversed());
+const person = {
+    name:"aditya",
+    lastname:"Nath"
+    
+}
+// person.name = "aman";
+// delete person.name;
+// console.log(person.name);
+// console.log(typeof arr);
